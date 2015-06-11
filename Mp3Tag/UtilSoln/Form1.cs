d91df35ef93de351a59a40b00b3deaf9a6b8a92d
GitBulk <giacatiensinh@hotmail.com>
2015-06-11 12:24:17 +0700

    Add
    Mp3 Tag

---
+++ Mp3Tag/UtilSoln/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UtilSoln
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string folderPath = @"C:\Downloads";
+            string[] filePaths = Directory.GetFiles(folderPath);
+            foreach (string filePath in filePaths)
+            {
+                TagLib.File f = TagLib.File.Create(filePath);
+                f.Tag.Copyright = "Git";
+                f.Tag.Performers = new string[] { "Git" };
+                //f.Tag.Album = "New Album Title";
+                f.Save();
+            }
+
+            //string path = @"C:\Downloads\03.mp3";
+            //TagLib.File f = TagLib.File.Create(path);
+            //f.Tag.Performers = new string[] { "Git " };
+            ////f.Tag.FirstArtist = "Git";
+            ////f.Tag.Album = "New Album Title";
+            //f.Save();
+
+            MessageBox.Show("Done");
+        }
+    }
+}
